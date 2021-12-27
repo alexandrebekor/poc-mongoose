8afc9bea5d6c81f8c6d42132c7f57cb63ce6e884
Alexandre Bekor <contato@alexandrebekor.com>
2021-12-27 16:01:35 -0300

    added edit and update series

--- controllers/series.js
+++ controllers/series.js
@@ -35,18 +35,32 @@ const store = async (Serie, req, res) => {
 const edit = async (Serie, req, res) => {
     const { id } = req.params
     try {
-        Serie.update({})
-        res.render('series/edit')
+        const serie = await Serie.findById(id)
+        res.render('series/edit', { serie })
     } catch (error) {
         res.send(error)
+    }
+}
+
+const update = async (Serie, req, res) => {
+    const { id } = req.params
+    const { title, description } = req.body
+    try {
+        const serie = await Serie.updateOne(
+            { _id: id },
+            { title, description }
+        )
+
+        res.redirect('/series')
+    } catch (error) {
+        res.send("error")
     }
 }
 
 const destroy = async (Serie, req, res) => {
     const { id } = req.params
     try {
-        const serie = await Serie.deleteOne({ id })
-        console.log(serie)
+        const serie = await Serie.deleteOne({ _id: id })
         res.redirect('/series')
     } catch (error) {
         res.redirect('/series')
@@ -59,5 +73,6 @@ module.exports = {
     create,
     store,
     edit,
+    update,
     destroy
 }
--- routes/series.js
+++ routes/series.js
@@ -8,6 +8,7 @@ router.get('/:id/serie', controller.find.bind(null, model))
 router.get('/create', controller.create)
 router.post('/create', controller.store.bind(null, model))
 router.get('/:id/edit', controller.edit.bind(null, model))
+router.post('/:id/edit', controller.update.bind(null, model))
 router.get('/:id/destroy', controller.destroy.bind(null, model))
 
 module.exports = router
